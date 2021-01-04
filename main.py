ba9b04d50ebda17dc5e05e52515af4a33fa680d5
maksim <maks.denv.terehov@gmail.com>
2021-01-04 18:30:55 +0300

    added webhooks

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import json
+import os
 
 import requests
 import telebot
+from flask import Flask, request
 
-bot = telebot.TeleBot('1565130854:AAECOdx2Xg-SAPqHgbahTlRthh2_waHnB1s')
+TOKEN = '1565130854:AAECOdx2Xg-SAPqHgbahTlRthh2_waHnB1s'
+bot = telebot.TeleBot(TOKEN)
 
 keyboard = telebot.types.ReplyKeyboardMarkup()
 keyboard.row('BTC/USD', 'BTC/RUB')
+
+
+server = Flask(__name__)
 
 
 def get_btc_price(currency: str):
@@ -32,5 +38,18 @@ def send_currency(message):
     bot.send_message(message.chat.id, answer)
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://denvy-telebot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
